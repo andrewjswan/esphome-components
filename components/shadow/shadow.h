6e7b8b71b324f0dc124fccab3e7e6ea20c2b3293
Andrew J.Swan <ajs.blackhole@gmail.com>
2024-09-17 21:51:06 +0300

    Version Bump

--- components/shadow/shadow.h
+++ components/shadow/shadow.h
@@ -4,7 +4,7 @@
 #include "esphome/core/component.h"
 #include "esphome/core/helpers.h"
 
-static const char *const SHADOW_VERSION = "2024.6.5";
+static const char *const SHADOW_VERSION = "2024.9.5";
 static const char *const TAG = "shadow";
 
 namespace esphome

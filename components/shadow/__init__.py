a9cb913e13b95bbd3a97e64dc80ffdef1ccaa251
Andrew J.Swan <ajs.blackhole@gmail.com>
2024-09-17 19:44:33 +0300

    Add Linter

--- components/shadow/__init__.py
+++ components/shadow/__init__.py
@@ -1,10 +1,11 @@
+"""Shadow component for ESPHome."""
+
 import logging
+
 import esphome.codegen as cg
 import esphome.config_validation as cv
-
-from esphome.core import CORE
-from esphome.const import CONF_ID, CONF_INTERVAL
 from esphome.components import script
+from esphome.const import CONF_ID, CONF_INTERVAL
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -14,29 +15,33 @@ DEPENDENCIES = ["script"]
 
 AUTO_LOAD = ["shadow"]
 
-logging.info(f"")
-logging.info(f"Load shadow component https://github.com/andrewjswan/esphome-components")
-logging.info(f"")
+logging.info("")
+logging.info("Load shadow component https://github.com/andrewjswan/esphome-components")
+logging.info("")
 
 shadow_ns = cg.esphome_ns.namespace("esphome")
 SHADOW_ = shadow_ns.class_("SHADOW", cg.Component)
 
 CONF_SCRIPT_ID = "script_id"
 
-SHADOW_SCHEMA = cv.Schema({
-  cv.Required(CONF_ID): cv.declare_id(SHADOW_),
-  cv.Required(CONF_SCRIPT_ID): cv.use_id(script),
-  cv.Optional(CONF_INTERVAL, default="60"): cv.templatable(cv.positive_int),
-})
+SHADOW_SCHEMA = cv.Schema(
+    {
+        cv.Required(CONF_ID): cv.declare_id(SHADOW_),
+        cv.Required(CONF_SCRIPT_ID): cv.use_id(script),
+        cv.Optional(CONF_INTERVAL, default="60"): cv.templatable(cv.positive_int),
+    },
+)
 
 CONFIG_SCHEMA = cv.All(SHADOW_SCHEMA)
 
-async def to_code(config):
-  var = cg.new_Pvariable(config[CONF_ID])
 
-  shadow_script = await cg.get_variable(config[CONF_SCRIPT_ID])
-  cg.add(var.set_script(shadow_script))
+async def to_code(config) -> None:  # noqa: ANN001
+    """Code generation entry point."""
+    var = cg.new_Pvariable(config[CONF_ID])
 
-  cg.add_define("SHADOW_INTERVAL", config[CONF_INTERVAL])
+    shadow_script = await cg.get_variable(config[CONF_SCRIPT_ID])
+    cg.add(var.set_script(shadow_script))
 
-  await cg.register_component(var, config)
+    cg.add_define("SHADOW_INTERVAL", config[CONF_INTERVAL])
+
+    await cg.register_component(var, config)

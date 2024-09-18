98a0af6a572b00c77133f91d93cd9c1c642a2383
Andrew J.Swan <ajs.blackhole@gmail.com>
2024-09-18 16:26:56 +0300

    C++ Lint

--- components/shadow/shadow.cpp
+++ components/shadow/shadow.cpp
@@ -1,15 +1,12 @@
 #include "esphome.h"
 
-namespace esphome
-{
-  void SHADOW::setup()
-  {
+namespace esphome {
+  void SHADOW::setup() {
     ESP_LOGCONFIG(TAG, "Setting up shadow...");
     this->start();
   }
 
-  void SHADOW::start()
-  {
+  void SHADOW::start() {
     xTaskCreatePinnedToCore(
       SHADOW::shadow_function, // Function to implement the task
       TAG,                     // Name of the task
@@ -22,21 +19,17 @@ namespace esphome
     ESP_LOGCONFIG(TAG, "Running script in shadow...");
   }
 
-  void SHADOW::shadow_function(void *params)
-  {
+  void SHADOW::shadow_function(void *params) {
     SHADOW *this_task = (SHADOW *) params;
-    for(;;)
-    {
+    for(;;) {
       vTaskDelay(SHADOW_INTERVAL * 1000 / portTICK_RATE_MS);
 
-      if (this_task->script == nullptr)
-      {
+      if (this_task->script == nullptr) {
         ESP_LOGE(TAG, "The script came out of the shadow. Skip.");
         continue;
       }
 
-      if (this_task->script->is_running())
-      {
+      if (this_task->script->is_running()) {
         continue;
       }
 
@@ -46,10 +39,8 @@ namespace esphome
     vTaskDelete( NULL );
   } // shadow_function()
 
-  void SHADOW::stop()
-  {
-    if (this->script != nullptr && this->script->is_running())
-    {
+  void SHADOW::stop() {
+    if (this->script != nullptr && this->script->is_running()) {
       this->script->stop();
     }
 
@@ -59,14 +50,12 @@ namespace esphome
     ESP_LOGCONFIG(TAG, "Everyone came out of the shadow.");
   }
 
-  void SHADOW::set_script(script::Script<> *script)
-  {
+  void SHADOW::set_script(script::Script<> *script) {
     this->script = script;
     ESP_LOGCONFIG(TAG, "Add script in the shadow...");
   }
 
-  void SHADOW::dump_config()
-  {
+  void SHADOW::dump_config() {
     ESP_LOGCONFIG(TAG, "Shadow version: %s", SHADOW_VERSION);
     ESP_LOGCONFIG(TAG, "      Interval: %ds", SHADOW_INTERVAL);
   }

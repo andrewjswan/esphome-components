023661fcc07b60c04053af9fcb97e3a3d7679a01
Andrew J.Swan <ajs.blackhole@gmail.com>
2024-09-18 16:36:49 +0300

    C++ Lint

--- components/shadow/shadow.h
+++ components/shadow/shadow.h
@@ -4,19 +4,19 @@ static const char *const SHADOW_VERSION = "2024.9.5";
 static const char *const TAG = "shadow";
 
 namespace esphome {
-  class SHADOW : public Component {
-    public:
-      void setup() override;
-      void start();
-      void stop();
+class SHADOW : public Component {
+ public:
+  void setup() override;
+  void start();
+  void stop();
 
-      void dump_config() override;
-      void set_script(script::Script<> *script);
+  void dump_config() override;
+  void set_script(script::Script<> *script);
 
-    protected:
-      TaskHandle_t shadow_handle = nullptr;
-      script::Script<> *script;
+ protected:
+  TaskHandle_t shadow_handle = nullptr;
+  script::Script<> *script;
 
-      static void shadow_function(void *params);
-  };
-}
+  static void shadow_function(void *params);
+};
+} // namespace esphome

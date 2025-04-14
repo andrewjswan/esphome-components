f7ea8534ad3c7a52752946db65e2d97a1549d716
Andrew J.Swan <ajs.blackhole@gmail.com>
2025-04-14 21:45:40 +0300

    C++ Lint (#39)
    * C++ Lint
    
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- components/nerdminer/monitor.cpp
+++ components/nerdminer/monitor.cpp
@@ -30,9 +30,8 @@ static std::list<double> s_hashrate_avg_list;
 static double s_hashrate_summ = 0.0;
 static uint8_t s_hashrate_recalc = 0;
 
-double getCurrentHashRate(unsigned long mElapsed)
-{
-  double hashrate = (double)elapsedKHs * 1000.0 / (double)mElapsed;
+double getCurrentHashRate(unsigned long mElapsed) {
+  double hashrate = (double) elapsedKHs * 1000.0 / (double) mElapsed;
 
   s_hashrate_summ += hashrate;
   s_hashrate_avg_list.push_back(hashrate);
@@ -42,15 +41,14 @@ double getCurrentHashRate(unsigned long mElapsed)
   }
 
   ++s_hashrate_recalc;
-  if (s_hashrate_recalc == 0)
-  {
+  if (s_hashrate_recalc == 0) {
     s_hashrate_summ = 0.0;
     for (auto itt = s_hashrate_avg_list.begin(); itt != s_hashrate_avg_list.end(); ++itt) {
       s_hashrate_summ += *itt;
     }
   }
 
-  double avg_hashrate = s_hashrate_summ / (double)s_hashrate_avg_list.size();
+  double avg_hashrate = s_hashrate_summ / (double) s_hashrate_avg_list.size();
   if (avg_hashrate < 0.0) {
     avg_hashrate = 0.0;
   }

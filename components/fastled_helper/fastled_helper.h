8125118ef42b290d9d22c92e3dee8902beb13a00
Andrew J.Swan <ajs.blackhole@gmail.com>
2025-10-08 18:53:06 +0300

    FastLED Helper: Rollback to FastLED 3.10.1

--- components/fastled_helper/fastled_helper.h
+++ components/fastled_helper/fastled_helper.h
@@ -3,7 +3,7 @@
 namespace esphome {
 namespace fastled_helper {
 
-static const char *const FASTLED_HELPER_VERSION = "2025.9.1";
+static const char *const FASTLED_HELPER_VERSION = "2025.10.1";
 static const char *const TAG = "fastled_helper";
 
 class FastledHelper : public Component {

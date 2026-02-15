399431e5cbec6e98002098af221ecaee95e31f8d
Andrew J.Swan <ajs.blackhole@gmail.com>
2026-02-15 14:28:06 +0200

    Shadow: Add `startup_delay` - delay of the first launch
    Add `MULTI_CONF` support
    
    **Breaking changes:**
    - Now the `script` starts immediately, without waiting for the `interval` to expire (unless a `startup delay` is specified).
    - Interval changed from `Int` to `Time`.

--- components/shadow/shadow.h
+++ components/shadow/shadow.h
@@ -11,7 +11,7 @@
 namespace esphome {
 namespace shadow {
 
-static const char *const SHADOW_VERSION = "2025.7.1";
+static const char *const SHADOW_VERSION = "2026.2.1";
 static const char *const TAG = "shadow";
 
 class Shadow : public Component
@@ -21,13 +21,17 @@ class Shadow : public Component
 #endif
 {
  public:
+  float get_setup_priority() const override { return setup_priority::PROCESSOR; }
+
   void setup() override;
   void start();
   void stop();
 
   void dump_config() override;
+
   void set_script(script::Script<> *script);
   void set_shadow_interval(uint8_t shadow_interval) { this->shadow_interval_ = shadow_interval; }
+  void set_startup_delay(uint8_t startup_delay) { this->startup_delay_ = startup_delay; }
 
 #ifdef USE_OTA_STATE_LISTENER
   void on_ota_global_state(ota::OTAState state, float progress, uint8_t error, ota::OTAComponent *comp) override;
@@ -37,6 +41,9 @@ class Shadow : public Component
   TaskHandle_t shadow_handle{nullptr};
   script::Script<> *script{nullptr};
   uint8_t shadow_interval_{60};
+  uint8_t startup_delay_{0};
+
+  void execute_script();
 
   static void shadow_function(void *params);
 };  // Shadow

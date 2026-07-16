a3232ab11956fa6286f977c8b63d5478f3a6cbe7
Andrew J.Swan <ajs.blackhole@gmail.com>
2026-07-16 23:13:27 +0300

    Update components/shadow/shadow.h
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- components/shadow/shadow.h
+++ components/shadow/shadow.h
@@ -16,7 +16,7 @@ static const char *const TAG = "shadow";
 class Shadow final : public Component
 #ifdef USE_OTA_STATE_LISTENER
     ,
-               public ota::OTAGlobalStateListener
+                     public ota::OTAGlobalStateListener
 #endif
 {
  public:
